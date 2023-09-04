b3171ab22964be5cb9cc0697fa23a9b6b232e80d
Evan Ishibashi <evan.ishibashi@gmail.com>
2023-09-04 12:10:10 -0700

    made cupcake class and first get route

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
-"""Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, jsonify, request, redirect, render_template
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///cupcakes')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+@app.get('/api/cupcakes')
+def get_all_cupcake_data():
+
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.get('api/cupcakes/<int:cupcake_id>')
+def get_single_cupcake(cupcake_id):
