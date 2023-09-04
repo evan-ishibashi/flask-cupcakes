48497c816396f9f5c1bdce0a408cb0ad1596d7ac
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-09-04 16:33:56 -0700

    slight changes to js file

--- static/app.js
+++ static/app.js
@@ -10,13 +10,15 @@ async function grabCupcakes() {
   const resp = await fetch('/api/cupcakes')
   const data = await resp.json()
 
-  data.cupcakes[0].flavor
-
-
-  for (let i = 0; i < data.cupcakes.length; i++) {
-    cupcakeList.push(data.cupcakes[i].flavor);
-  }
-
-  $cupcakeList.append(cupcakeList)
+  return data.cupcakes;
+  // returns [(cupcake item1), (cupcake item2)]
 }
 
+// data.cupcakes[0].flavor
+
+
+  // for (let i = 0; i < data.cupcakes.length; i++) {
+  //   cupcakeList.push(data.cupcakes[i].flavor);
+  // }
+
+  // $cupcakeList.append(cupcakeList)
