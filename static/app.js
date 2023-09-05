b273eaf766ae3b761792770814ef04985b6eb4b4
Evan Ishibashi <evan.ishibashi@gmail.com>
2023-09-04 17:03:00 -0700

    wrote js functions for getting and displaying cupcakes

--- static/app.js
+++ static/app.js
@@ -3,7 +3,6 @@
 const $cupcakeList = $('#cupcake-list');
 const $cupcakeForm = $('#cupcake-form');
 
-const cupcakeList = [];
 
 async function grabCupcakes() {
 
@@ -14,11 +13,52 @@ async function grabCupcakes() {
   // returns [(cupcake item1), (cupcake item2)]
 }
 
-// data.cupcakes[0].flavor
+/** Generates/Appends HTML of Each Cupcake */
+function displayCupcakes(cupcakeData) {
+  $cupcakeList.empty();
+
+  for (const cupcake of cupcakeData) {
+    const $cupcake = $(`
+        <li id="${cupcake.flavor}">
+         <div>
+           <img
+              src="${cupcake.image_url}"
+              alt="${cupcake.flavor}"
+              >
+           <div class="media-body">
+             <h5 class="text-primary"><b>Cupcake Details</b></h5>
+             <div><small>Cupcake Flavor: ${cupcake.flavor}</small></div>
+             <div><small>Cupcake Rating: ${cupcake.rating}</small></div>
+             <div><small>Cupcake Size: ${cupcake.size}</small></div>
+           </div>
+         </div>
+       </li>
+      `);
+
+    $cupcakeList.append($cupcake);
+  }
+}
+
+async function displayCupcakeList(){
 
 
-  // for (let i = 0; i < data.cupcakes.length; i++) {
-  //   cupcakeList.push(data.cupcakes[i].flavor);
-  // }
+  const cupcakeData = await grabCupcakes();
 
-  // $cupcakeList.append(cupcakeList)
+  displayCupcakes(cupcakeData);
+
+}
+
+displayCupcakeList();
+/**Handles Cupcake Submission Form */
+async function addCupcake(evt){
+  evt.preventDefault()
+
+  const flavor = $('#flavor').val();
+  const rating = $('#rating').val();
+  const size = $('#size').val();
+  const image = $('#image').val();
+
+  const resp = await fetch('/api/cupcakes')
+
+
+}
